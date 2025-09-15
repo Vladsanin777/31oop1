49b2fae78435fd0099b2d5b5363d02085038435b
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2025-09-16 01:13:25 +0400

    ready lab5

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,7 +1,9 @@
+#include <vector>
 #include <cmath>
 #include <iostream>
 #include <ostream>
 
+template<typename Tp>
 class point {
 private:
     Tp _x, _y;
@@ -19,7 +21,7 @@ public:
             const point<Tp> &point3) {
         Tp a{point2.len(point3)}, b{point3.len(*this)}, \
             c{point2.len(*this)};
-        return acos((b * b + c * c - a * a) / (2 * b * c));
+        return acos((b * b + c * c - a * a) / (2 * b * c)) * 180 / M_PI;
     }
 };
 
@@ -28,9 +30,11 @@ class triangle {
 private:
     point<Tp> _a, _b, _c;
 public:
-    triangle(a, b, c) : _a{a}, _b{b}, _c{c} {}
+    triangle(point<Tp> a, point<Tp>b, point<Tp> c) : \
+        _a{a}, _b{b}, _c{c} {}
     virtual bool is(void) {
-        return _a + _b < _c || _a + _c < _b || _b + _c < _a;
+        Tp a{lenA()}, b{lenB()}, c{lenC()};
+        return a + b > c && a + c > b && b + c > a;
     }
     Tp lenA(void) {
         return _c.len(_b);
@@ -47,29 +51,46 @@ public:
     Tp angleB(void) {
         return _b.angle(_a, _c);
     }
-    Tp angelC(void) {
-        return _c.angel(_b, _a);
+    Tp angleC(void) {
+        return _c.angle(_b, _a);
     }
     Tp perimetr(void) {
         return lenA() + lenB() + lenC();
     }
     Tp square(void) {
-        return Tp{(lenA() * lenB() * sin(angelC()))} / 2;
+        return ((Tp)(lenA() * lenB() * sin(angleC()))) / 2;
     }
 };
-
-class right_triangle : public triangle {
+template<typename Tp>
+class right_triangle : public triangle<Tp> {
 public:
+    right_triangle(point<Tp> a, point<Tp> b, point<Tp> c) : \
+        triangle<Tp>{a, b, c} {}
     bool is(void) override {
-        Tp len_a{lenA()}, len_b{lenB()}, len_c{lenC()};
-        len_a <<= 1, len_b <<= 1, len_c <<= 1;
+        Tp len_a{this->lenA()}, len_b{this->lenB()}, \
+            len_c{this->lenC()};
+        len_a *= len_a, len_b *= len_b, len_c *= len_c;
         return len_a + len_b == len_c || \
             len_b + len_c == len_a || \
             len_c + len_a == len_b;
     }
 };
 
-int main(void) {
-    return 0;
+template<typename Tp>
+void test(std::vector<Tp> &vec) {
+    for (auto &element : vec)
+        std::cout << "Is " << element.is() << "\tlenA " << \
+            element.lenA() << "\tlenB " << element.lenB() << "\tlenC " << \
+            element.lenC() << "\tangleA " << element.angleA() << "\tangelB " << \
+            element.angleB() << "\tangelC " << element.angleC() << "\tperimeter " << \
+            element.perimetr() << "\tsquare " << element.square() << std::endl;
+    std::cout << "next" << std::endl;
+}
 
+int main(void) {
+    std::vector<triangle<int>> N{{{3, 9}, {4, 5}, {9, 0}}, {{9, 0}, {9, 1}, {8, 3}}};
+    std::vector<right_triangle<int>> M{{{3, 4}, {5, 8}, {9, 2}}, {{9, 0}, {1, 2}, {9, 8}}};
+    test(N);
+    test(M);
+    return 0;
 }
