b2aee80588660e6fee4e3adb8a6c1e84bff67d93
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2025-09-19 07:59:08 +0400

    ready lab7

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,3 +1,5 @@
+#include <string>
+#include <set>
 using u8 = unsigned char;
 
 class Time {
@@ -19,8 +21,8 @@ private:
     Time _dayBegin{}, _dayEnd{}, \
         _dinnerBegin{}, _dinnerEnd{};
 public:
-    Schedule(Time dayBegin, Time dayEnd, \
-            Time dinnerBegin, Time dinnerEnd) : \
+    Schedule(Time dayBegin = Time{}, Time dayEnd = Time{}, \
+            Time dinnerBegin = Time{}, Time dinnerEnd = Time{}) : \
             _dayBegin{dayBegin}, _dayEnd{dayEnd}, \
             _dinnerBegin{dayBegin}, _dinnerEnd{dinnerEnd} {}
     Time getDayBegin(void) { return _dayBegin; }
@@ -32,3 +34,105 @@ public:
     void setDinnerBegin(Time dinnerBegin) { _dinnerBegin = dinnerBegin; }
     void setDinnerEnd(Time dinnerEnd) { _dinnerEnd = dinnerEnd; }
 };
+
+class Worker {
+    std::set<Schedule *> _schedule;
+    Worker(std::set<Schedule *> schedule = {}) : \
+        _schedule{schedule} {}
+    void addSchedule(Schedule *newSchedule) {
+        _schedule.insert(newSchedule);
+    }
+    void removeSchedule(Schedule *removeSchedule) {
+        _schedule.erase(removeSchedule);
+    }
+};
+
+class Product {
+    unsigned _weight;
+    std::string _name;
+    Product(unsigned weight, std::string name) : \
+        _weight{weight}, _name{name} {}
+    void setWeight(unsigned newWeight) {
+        _weight = newWeight;
+    }
+    void setName(std::string newName) {
+        _name = newName;
+    }
+    unsigned getWeight(void) const {
+        return _weight;
+    }
+    std::string getName(void) {
+        return _name;
+    }
+};
+
+class Order {
+    unsigned _number;
+    std::set<Product *> _products;
+    Order(unsigned number, \
+            std::set<Product *> product = {}) : \
+        _number{number}, _products{product} {}
+    unsigned getNumber(void) {
+        return _number;
+    }
+    void setNumber(unsigned newNumber) {
+        _number = newNumber;
+    }
+    void addProduct(Product *newProduct) {
+        _products.insert(newProduct);
+    }
+    void removeProduct(Product *product) {
+        _products.erase(product);
+    }
+};
+
+class Buyer {
+    long long int _numberCreaditCart{0};
+    std::string _name;
+    std::set<Order *> _orders;
+    Buyer(long long int numberCreaditCart = 0, \
+        std::string name = "") : _numberCreaditCart{ \
+        numberCreaditCart}, _name{name} {}
+    void setName(std::string newName) {
+        _name = newName;
+    }
+    std::string getName(void) const {
+        return _name;
+    }
+    void setNumberCreditCart(long long int \
+            newNumberCreaditCart) {
+        _numberCreaditCart = newNumberCreaditCart;
+    }
+    long long int getNumberCreditCart(void) const {
+        return _numberCreaditCart;
+    }
+    void addOrder(Order *order) {
+        _orders.insert(order);
+    }
+    void removeOrder(Order *order) {
+        _orders.erase(order);
+    }
+};
+
+class Delivery {
+    Order * _order;
+    Buyer * _buyer;
+    Delivery(Order * order, Buyer * buyer) : \
+        _order{order}, _buyer{buyer} {}
+    void setOrder(Order * order) {
+        _order = order;
+    }
+    void setBuyer(Buyer * buyer) {
+        _buyer = buyer;
+    }
+    Order * getOrder(void) const {
+        return _order;
+    }
+    Buyer * getBuyer(void) const {
+        return _buyer;
+    }
+};
+
+int main(void) {
+    return 0;
+}
