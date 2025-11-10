a18ce27889666b8fb9010259fa877aa754908546
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2025-11-11 00:45:47 +0400

    edit sam3

--- sam3/main.cpp
+++ sam3/main.cpp
@@ -4,6 +4,7 @@
 #include <iterator>
 #include <queue>
 #include <deque>
+#include <cstring>
 
 
 using u8 = unsigned char;
@@ -13,15 +14,17 @@ class event {
 private:
     u8 _day, _month;
     u16 _year;
-    std::string _label;
+    char _label[50];
 public:
     event(u8 day, u8 month = 0, u16 year = 0, \
-            std::string label = {}) : _day{day}, \
-            _month{month}, _year{year}, _label{label} {}
+            const char * label = "") : _day{day}, \
+            _month{month}, _year{year}, _label{} {
+        strcpy(_label, label);
+    }
     event() : _day{0}, _month{0}, _year{0}, _label{""} {}
     event(const event &eventOld) : _day{eventOld._day}, \
         _month{eventOld._month}, _year{eventOld._year}, \
-        _label{eventOld._label} {}
+        _label{} {strcpy(_label, eventOld._label);}
     u8 getDay(void) const {
         return _day;
     }
@@ -31,7 +34,7 @@ public:
     u8 getYear(void) const {
         return _year;
     }
-    std::string getLabel(void) const {
+    const char * getLabel(void) const {
         return _label;
     }
     void setDay(const u8 &day) {
@@ -43,60 +46,107 @@ public:
     void setYear(const u16 &year) {
         _year = year;
     }
-    void setLabel(const std::string &label) {
-        _label = label;
-    }
     friend std::ostream &operator<<(std::ostream &out, const event &event1) {
         out << "event address: " << &event1 << ", day: " << (short)event1._day << \
             ", month: " << (short)event1._month << ", year: " << event1._year << \
-            ", label: " << event1._label << std::endl;
+            ", label: " << ((strlen(event1._label) == 0) ? "\"\"" : event1._label) << std::endl;
         return out;
     }
 };
 
-class deque : public std::deque<event> {
+template<typename T>
+class deque {
+private:
+    size_t _size{0};
+    T *_arr{nullptr};
 public:
-    deque(std::initializer_list<event> il) : std::deque<event>() {}
-    deque searchDay(const u8 &day) const {
-        deque result{};
-        const typename std::deque<event>::const_iterator itEnd { this->cend() };
-        for (typename std::deque<event>::const_iterator it { this->cbegin() }; \
+    deque(void) : _size{0}, _arr{nullptr} {}
+    deque(T* arr, size_t size) : _size{size}, _arr{malloc(sizeof(T) * size)} {
+        _arr = (T*)memcpy((char *)_arr, (char *)arr, sizeof(T) * size);
+    }
+    deque(const deque<T> &deq) : _size{deq._size}, \
+            _arr{(T *)malloc(sizeof(T) * deq._size)} {
+        _arr = (T*)memcpy((char *)_arr, (char *)deq._arr, sizeof(T) * deq._size);
+    }
+    deque<T> searchDay(const u8 &day) const {
+        deque<T> result{};
+        const T* const itEnd { end() };
+        for (const T *it { begin() }; \
             it != itEnd; it++) if ((*it).getDay() == day) result.push_back(*it);
         return result;
     }
-    deque searchMonth(const u8 &month) const {
-        deque result{};
-        const typename std::deque<event>::const_iterator itEnd { this->cend() };
-        for (typename std::deque<event>::const_iterator it { this->cbegin() }; \
+    void push_back(T newVal) {
+        size_t oldSize {_size};
+        resize(_size+1);
+        _arr[oldSize] = newVal;
+    }
+    void push_top(T newVal) {
+        resize(_size+1);
+        memcpy(_arr + 1, _arr, sizeof(T) * _size - 1);
+        *_arr = newVal;
+    }
+    T pop_back(void) {
+        T ret {_arr[_size-1]};
+        resize(_size-1);
+        return ret;
+    }
+    T pop_top(void) {
+        T ret {_arr[0]};
+        memcpy(_arr, _arr + 1, _size - 1);
+        resize(_size-1);
+        return ret;
+    }
+    const T *end(void) const {
+        return _arr + _size;
+    }
+    const T *begin(void) const {
+        return _arr;
+    }
+    bool empty(void) {
+        return !_size;
+    }
+    deque<T> searchMonth(const u8 &month) const {
+        deque<T> result{};
+        const T* const itEnd { end() };
+        for (const T *it{ begin() }; \
             it != itEnd; it++) if ((*it).getMonth() == month) result.push_back(*it);
         return result;
     }
-    deque searchYear(const u16 &year) const {
-        deque result{};
-        const typename std::deque<event>::const_iterator itEnd { this->cend() };
-        for (typename std::deque<event>::const_iterator it { this->cbegin() }; \
+    deque<T> searchYear(const u16 &year) const {
+        deque<T> result{};
+        const T* const itEnd { end() };
+        for (const T *it{begin()}; \
             it != itEnd; it++) if ((*it).getYear() == year) result.push_back(*it);
         return result;
     }
-    deque searchLabel(const std::string &label) const {
-        deque result{};
-        const typename std::deque<event>::const_iterator itEnd { this->cend() };
-        for (typename std::deque<event>::const_iterator it { this->cbegin() }; \
-            it != itEnd; it++) if ((*it).getLabel() == label) result.push_back(*it);
+    deque<T> searchLabel(const char *label) const {
+        deque<T> result{};
+        const T* const itEnd { end() };
+        for (const T *it{begin()}; \
+            it != itEnd; it++) if (!strcmp(it->getLabel(), label)) result.push_back(*it);
         return result;
     }
-    friend std::ostream &operator<<(std::ostream &out, const deque &deque1) {
+
+    friend std::ostream &operator<<(std::ostream &out, const deque<T> &deque1) {
         out << "deque address: " << &deque1 << std::endl;
-        const typename std::deque<event>::const_iterator itEnd { deque1.cend() };
-        for (typename std::deque<event>::const_iterator it { deque1.cbegin() }; \
+        const T* const itEnd { deque1.end() };
+        for (const T * it {deque1.begin()}; \
             it != itEnd; it++) out << *it;
         out << std::endl;
         return out;
     }
+    ~deque(void) {
+        if (_arr) free(_arr);
+    }
+private:
+    void resize(size_t newSize) {
+        _size = newSize;
+        _arr = (T *)realloc(_arr, sizeof(T) * newSize);
+    }
 };
 
 int main(void) {
-    deque deque1 {};
+    deque<event> deque1 {};
     deque1.push_back(event{}); deque1.push_back(event{20, 5, 2008, "jk"});
     deque1.push_back({12, 2, 3032, "al"}); deque1.push_back(event{});
     std::cout << "deque orig: " << deque1 << std::endl;
